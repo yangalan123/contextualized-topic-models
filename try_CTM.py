d979e232329e6066179bc18d3a300bc0dc12b36e
yangalan123 <1049909407@qq.com>
2021-01-09 00:55:14 -0600

    update server side codes

--- try_CTM.py
+++ try_CTM.py
@@ -3,7 +3,7 @@ from contextualized_topic_models.utils.data_preparation import QuickText
 from contextualized_topic_models.utils.data_preparation import bert_embeddings_from_file
 from contextualized_topic_models.datasets.dataset import CTMDataset
 from contextualized_topic_models.utils.preprocessing import WhiteSpacePreprocessing
-documents = ["Are you happy?", "No I am not."]
+documents = ["I write a book and eat a cake.", "I am really happy today because I met with Tom."]
 sp = WhiteSpacePreprocessing(documents)
 preprocessed_docs, unpreprocessed_doc, vocab = sp.preprocess()
 
@@ -17,4 +17,4 @@ ctm = CombinedTM(input_size=len(qt.vocab), bert_input_size=768, n_components=2)
 
 ctm.fit(training_dataset) # run the model
 
-ctm.get_topics()
+print(ctm.get_topics())
